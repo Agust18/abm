0ea63ee59d06fe74ead9972835745cb1549a80de
Agust18 <agustincamera16@hotmail.com>
2024-09-26 19:17:24 -0300

    first commit

---
+++ db/db.php
@@ -0,0 +1,20 @@
+<?php
+//para conectarnos necesitamos 
+//SERVIDOR O IP , USUARIO , PASSWORD ,DATABASE.
+$servidor = "utn-mysql"; // o 127.0.0.1
+$usuario = "root"; 
+$password = "password";
+$database = "base";
+
+//Declarar conexion con mysqly 
+//declarar los parametros en el mismo orden declarado arriba
+$conx = new mysqli($servidor,$usuario,$password,$database);
+//calcular un error
+if ($conx->connect_error){
+    echo "error: ".$conx->connect_error;
+
+}
+
+
+
+?>
